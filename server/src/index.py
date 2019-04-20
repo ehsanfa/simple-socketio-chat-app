c17ff15866b0d362f4e0afff8208091f3075ab1e
Ehsan Fazeli <ehsan.fazeli@zoodfood.com>
2019-04-20 18:48:40 +0430

    add load balancig to nginx

--- server/src/index.py
+++ server/src/index.py
@@ -2,6 +2,15 @@ import os
 from aiohttp import web
 import socketio
 
+while(True):
+	try:
+		mgr = socketio.AsyncRedisManager('redis://redis')
+	except:
+		print("redis is not responding, trying again in 1 sec...")
+		sleep(1)
+		continue
+	break
+
 mgr = socketio.AsyncRedisManager('redis://redis')
 sio = socketio.AsyncServer(client_manager=mgr)
 app = web.Application()
@@ -10,7 +19,7 @@ sio.attach(app)
 async def index(request):
 	"""Serve the client-side application."""
 	with open('index.html') as f:
-		return web.Response(text=f.read(), content_type='text/html')
+		return web.Response(text=os.environ["SERVICE_NAME"], content_type='text/html')
 
 @sio.on('connect', namespace='/chat')
 def connect(sid, environ):
@@ -28,7 +37,7 @@ async def leave_room(sid, data):
 
 @sio.on('chat message', namespace='/chat')
 async def message(sid, data):
-	print("message ", data)
+	print("message ", data, os.environ["SERVICE_NAME"])
 	await sio.emit('reply', {'data': data}, room=data['room'], namespace='/chat', skip_sid=sid)
 	print("after await")
 
